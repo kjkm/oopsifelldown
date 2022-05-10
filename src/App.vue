3d6179713c77a22c0d2c6869d463dee46299ed5a
Kieran <kimmurkj@plu.edu>
2022-05-10 13:06:32 -0700

    init: set up our project!
    Initialized new VueJS project.
    Added BabylonJS to dependencies.
    Set up initial scene.

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import BabylonExamples from "@/components/BabylonExamples.vue";
+</script>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  export default defineComponent({
+    name: "App",
+    mounted() {
+      const canvas = document.querySelector('canvas');
+    },
+  });
+</script>
+
+<template>
+  <BabylonExamples msg="yo" />
+</template>
+
---
+++ src/components/BabylonExamples.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string;
+}>();
+</script>
+
+<template>
+  <div>
+    <h3>Babylon Examples</h3>
+    <canvas></canvas>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import { BasicScene } from '@/babylon/BasicScene';
+  export default defineComponent({
+    name: "BabylonExamples",
+    mounted() {
+      const canvas = document.querySelector('canvas')!;
+      new BasicScene(canvas); 
+    },
+  })
+</script>
+
+<style scoped>
+  canvas {
+    width: 70%;
+    height: 70%;
+  }
+</style>
