b76a45ca06bf52125790bf1d03dc95d7bbfc2774
Kieran <kimmurkj@plu.edu>
2022-05-10 19:19:13 -0700

    feat: add walls to map
    Add constants to control map and wall dimensions

--- src/babylon/BasicScene.ts
+++ src/babylon/BasicScene.ts
@@ -2,6 +2,11 @@ import * as BABYLON from '@babylonjs/core';
 
 export class BasicScene {
 
+    MAP_WIDTH: number = 100;
+    MAP_DEPTH: number = 100;
+    WALL_WIDTH: number = 1;
+    WALL_HEIGHT: number = 15;
+
     scene: BABYLON.Scene;
     engine: BABYLON.Engine;
 
@@ -21,8 +26,18 @@ export class BasicScene {
         camera.attachControl();
         const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), this.scene);
         light.intensity = 0.5;
-        const ground = BABYLON.MeshBuilder.CreateGround("ground1", {width:100, height:100}, this.scene);
+        const ground = BABYLON.MeshBuilder.CreateGround("ground1", {width:this.MAP_WIDTH, height:this.MAP_DEPTH}, this.scene);
         const sphere = BABYLON.MeshBuilder.CreateSphere("sphere1", {diameter:1}, this.scene);
+        const walls: BABYLON.Mesh[] = [];
+        walls[0] = BABYLON.MeshBuilder.CreateBox("wall1", {height:this.WALL_HEIGHT, width:this.MAP_WIDTH, depth:this.WALL_WIDTH}, this.scene);
+        walls[0].position = new BABYLON.Vector3(0, 0, this.MAP_DEPTH/2 - this.WALL_WIDTH/2);
+        walls[1] = BABYLON.MeshBuilder.CreateBox("wall2", {height:this.WALL_HEIGHT, width:this.WALL_WIDTH, depth:this.MAP_DEPTH}, this.scene);
+        walls[1].position = new BABYLON.Vector3(this.MAP_WIDTH/2 - this.WALL_WIDTH/2, 0, 0);
+        walls[2] = BABYLON.MeshBuilder.CreateBox("wall3", {height:this.WALL_HEIGHT, width:this.MAP_WIDTH, depth:this.WALL_WIDTH}, this.scene);
+        walls[2].position = new BABYLON.Vector3(0, 0, -this.MAP_DEPTH/2 + this.WALL_WIDTH/2);
+        walls[3] = BABYLON.MeshBuilder.CreateBox("wall4", {height:this.WALL_HEIGHT, width:this.WALL_WIDTH, depth:this.MAP_DEPTH}, this.scene);
+        walls[3].position = new BABYLON.Vector3(-this.MAP_WIDTH/2 + this.WALL_WIDTH/2, 0, 0);
+
         sphere.position = new BABYLON.Vector3(0, 1, 0);
         camera.position = new BABYLON.Vector3(0, 1, -10);
         return scene;
