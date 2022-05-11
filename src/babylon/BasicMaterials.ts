918fb4610744073ac99dc6dbf6bc67475a85d0c0
loganmargo <logan.margo@plu.edu>
2022-05-10 21:06:48 -0700

    added material folders
    added brick texture to ground

--- src/babylon/BasicMaterials.ts
+++ src/babylon/BasicMaterials.ts
@@ -1,6 +1,12 @@
+import { StandardMaterial, Texture } from '@babylonjs/core';
 import * as BABYLON from '@babylonjs/core';
 
 export class BasicMaterials {
+
+    MAP_WIDTH: number = 100;
+    MAP_DEPTH: number = 100;
+    WALL_WIDTH: number = 1;
+    WALL_HEIGHT: number = 15;
 
     scene: BABYLON.Scene;
     engine: BABYLON.Engine;
@@ -19,12 +25,74 @@ export class BasicMaterials {
         const scene = new BABYLON.Scene(this.engine);
         const camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 1, 0), this.scene);
         camera.attachControl();
-        const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), this.scene);
-        light.intensity = 0.5;
-        const ground = BABYLON.MeshBuilder.CreateGround("ground1", {width:100, height:100}, this.scene);
+        camera.speed = 0.25; 
+
         const sphere = BABYLON.MeshBuilder.CreateSphere("sphere1", {diameter:1}, this.scene);
         sphere.position = new BABYLON.Vector3(0, 1, 0);
         camera.position = new BABYLON.Vector3(0, 1, -10);
+        //sphere.material = this.CreateSphereMaterial(); 
+
+        const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), this.scene);
+        light.intensity = 1;
+
+        const ground = BABYLON.MeshBuilder.CreateGround("ground1", {width:this.MAP_WIDTH, height:this.MAP_DEPTH}, this.scene);
+        ground.material = this.CreateGroundMaterial(); 
+        
+        const walls: BABYLON.Mesh[] = [];
+        walls[0] = BABYLON.MeshBuilder.CreateBox("wall1", {height:this.WALL_HEIGHT, width:this.MAP_WIDTH, depth:this.WALL_WIDTH}, this.scene);
+        walls[0].position = new BABYLON.Vector3(0, 0, this.MAP_DEPTH/2 - this.WALL_WIDTH/2);
+        walls[1] = BABYLON.MeshBuilder.CreateBox("wall2", {height:this.WALL_HEIGHT, width:this.WALL_WIDTH, depth:this.MAP_DEPTH}, this.scene);
+        walls[1].position = new BABYLON.Vector3(this.MAP_WIDTH/2 - this.WALL_WIDTH/2, 0, 0);
+        walls[2] = BABYLON.MeshBuilder.CreateBox("wall3", {height:this.WALL_HEIGHT, width:this.MAP_WIDTH, depth:this.WALL_WIDTH}, this.scene);
+        walls[2].position = new BABYLON.Vector3(0, 0, -this.MAP_DEPTH/2 + this.WALL_WIDTH/2);
+        walls[3] = BABYLON.MeshBuilder.CreateBox("wall4", {height:this.WALL_HEIGHT, width:this.WALL_WIDTH, depth:this.MAP_DEPTH}, this.scene);
+        walls[3].position = new BABYLON.Vector3(-this.MAP_WIDTH/2 + this.WALL_WIDTH/2, 0, 0);
+
+        var skybox = BABYLON.MeshBuilder.CreateBox("skyBox", {size:1000.0}, this.scene);
+        var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", this.scene);
+        skyboxMaterial.backFaceCulling = false;
+        skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("textures/skybox", this.scene);
+        skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+        skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+        skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+        //skyboxMaterial.disableLighting = true;
+        skybox.material = skyboxMaterial;
+
+
         return scene;
     }
+
+    CreateGroundMaterial(): StandardMaterial {
+        const groundMaterial = new StandardMaterial("groundMaterial", this.scene); 
+        const uvScale = 4;
+        const texArr: Texture[] = []; 
+
+        const diffuseTex = new Texture("./textures/brick/brick_diffuse.jpg", this.scene);
+        groundMaterial.diffuseTexture = diffuseTex; 
+        texArr.push(diffuseTex); 
+
+        const normalTex = new Texture("./textures/brick/brick_normal.jpg", this.scene);
+        groundMaterial.bumpTexture = normalTex; 
+        texArr.push(normalTex);
+
+        const aoTex = new Texture("./textures/brick/brick_ao.jpg", this.scene);
+        groundMaterial.ambientTexture = aoTex; 
+        texArr.push(aoTex);
+
+        const specTex = new Texture("./textures/brick/brick_spec.jpg", this.scene);
+        groundMaterial.specularTexture = specTex; 
+        texArr.push(specTex);
+
+        texArr.forEach((tex)=>{
+            tex.uScale = uvScale;
+            tex.vScale = uvScale; 
+        })
+
+        return groundMaterial; 
+    }
+
+    //CreateSphereMaterial(): StandardMaterial {
+    // }
+
+
 }
