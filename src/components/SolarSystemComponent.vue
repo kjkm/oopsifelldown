4833df3f6084156485a440c9ad69927a8c97c9c5
loganmargo <logan.margo@plu.edu>
2022-05-11 18:20:47 -0700

    created solar vue file

---
+++ src/components/SolarSystemComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>Solar</h3>
+    <canvas></canvas>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { SolarSystemScene } from "@/babylon/SolarSystemScene";
+
+export default defineComponent({
+  name: "SolarSystemComponent",
+  mounted() {
+    const canvas = document.querySelector("canvas")!;
+    new BasicScene(canvas);
+  },
+});
+</script>
+
+<style scoped>
+canvas {
+  width: 90%;
+  height: 90%;
+}
+</style>
