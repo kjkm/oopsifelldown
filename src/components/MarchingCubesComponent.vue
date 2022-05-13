a4720507050194eacea3a04261fcdf1f4db76f78
Kieran <kimmurkj@plu.edu>
2022-05-13 12:18:15 -0700

    refactor: refactor marching cubes
    Add variables to marching cubes class to reduce number of parameters
    for each function.
    Started implementing mesh generation.

--- src/components/MarchingCubesComponent.vue
+++ src/components/MarchingCubesComponent.vue
@@ -8,12 +8,13 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import { MarchingCubes } from "@/babylon/MarchingCubes";
+import * as BABYLON from "@babylonjs/core";
 
 export default defineComponent({
     name: "MarchingCubesComponent",
     mounted() {
         const canvas = document.querySelector("canvas")!;
-        new MarchingCubes(canvas);
+        new MarchingCubes(canvas, new BABYLON.Vector3(0, 0, 0), new BABYLON.Vector3(16, 16, 16), 4, 500);
     },
 });
 </script>
