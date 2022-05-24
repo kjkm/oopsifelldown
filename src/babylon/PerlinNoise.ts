21b3ffdc47998e8c39f914472f04234742cd71ea
Kieran <kimmurkj@plu.edu>
2022-05-23 18:54:55 -0700

    fix: give planets a border so the mesh doesn't get cut off by the end of a chunk

--- src/babylon/PerlinNoise.ts
+++ src/babylon/PerlinNoise.ts
@@ -70,7 +70,8 @@ export class PerlinNoise2D{
                 for (let k = 0; k < this.nodes; k++) {
                     //const point = new Vector3(i, j - 16, k)
                     const radius = 15;
-                    const point = new Vector3(i - radius , j, k - radius);
+                    const border = 5;
+                    const point = new Vector3(i - radius - border , j - border, k - radius - border);
                     //space[i][j][k] = -point.y;
 
                     space[i][j][k] = radius - point.subtract(new Vector3(0, radius, 0)).length();
